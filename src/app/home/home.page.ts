95751cf577c0dd3774cc8af95a8409981278000b
ant <34335225+antconsales@users.noreply.github.com>
2021-05-10 14:51:10 +0200

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  myVarv : string = "Ciao Mondo!";
+  todos = ['Buy Milk', 'Learn Ionic'];
+  newTodo= '';
+  myImage = null;
+  
+  constructor(private router: Router, private camera: Camera) {}
+  
+  captureImage() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+  
+  addTodo() {
+    this.todos.push(this.newTodo);
+    this.newTodo = '';
+    console.log('todos:', this.todos);
+  }
+ 
+  foo() {
+    return 'Hello Ionic';
+
+  }
+
+  openPage() {
+    this.router.navigateByUrl('/details');
+
+  }
+}
